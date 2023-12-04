cfdb9116aa49446a5d36f99fab27ed8203cbfaba
miku39 <1716406895@qq.com>
2023-12-04 03:41:47 -0600

    update

--- main.py
+++ main.py
@@ -11,6 +11,36 @@ import collections
 import argparse
 
 times = 1
+
+def easy_time_prob():
+    global times
+    code1 = util.problem_generator.codeGenerator(times, "easy", "time")
+    times += 1
+    code2 = util.modification.codeModification(code1, "time")
+    time_all = [999,999]
+    if util.compare_pyres.compare_code_output(code1, code2):
+        time_all = util.time_compare.time_execution(code1, code2)
+        flag = time_all[0]>time_all[1]
+    else:
+        flag = False
+    need_to_be_written = [str(flag), "Easy", "Time", str(time_all[0]), str(time_all[1])]
+    return need_to_be_written
+
+
+def easy_mem_prob():
+    global times
+    code1 = util.problem_generator.codeGenerator(times, "easy", "space")
+    times += 1
+    code2 = util.modification.codeModification(code1, "space")
+    mem_all = [999,999]
+    if util.compare_pyres.compare_code_output(code1, code2):
+        mem_all = util.memory_compare.compare_memory_usage(code1, code2)
+        flag = mem_all[0]>mem_all[1]
+    else:
+        flag = False
+    need_to_be_written = [str(flag), "Easy", "Space", str(mem_all[0]), str(mem_all[1])]
+    return need_to_be_written
+
 def med_time_prob():
     global times
     code1 = util.problem_generator.codeGenerator(times, "medium", "time")
@@ -37,7 +67,7 @@ def med_mem_prob():
         flag = mem_all[0]>mem_all[1]
     else:
         flag = False
-    need_to_be_written = [str(flag), "Medium", "Memory", str(mem_all[0]), str(mem_all[1])]
+    need_to_be_written = [str(flag), "Medium", "Space", str(mem_all[0]), str(mem_all[1])]
     return need_to_be_written
 
 
@@ -67,13 +97,20 @@ def hard_mem_prob():
         flag = mem_all[0]>mem_all[1]
     else:
         flag = False
-    need_to_be_written = [str(flag), "Hard", "Memory", str(mem_all[0]), str(mem_all[1])]
+    need_to_be_written = [str(flag), "Hard", "Space", str(mem_all[0]), str(mem_all[1])]
     return need_to_be_written
 
-
+def easy_immi():
+    global times
+    code1 = util.problem_generator.codeGenerator(times, "easy", "space")
+    times += 1
+    code2 = util.immigration.codeImmigration(code1, "c++")
+    flag = util.compare_res.compile_and_compare(code2, code1)
+    need_to_be_written = [str(flag), "Easy",  "Immigration", "Python", "C++"]
+    return need_to_be_written
 def med_immi():
     global times
-    code1 = util.problem_generator.codeGenerator(times, "medium", "memory")
+    code1 = util.problem_generator.codeGenerator(times, "medium", "space")
     times += 1
     code2 = util.immigration.codeImmigration(code1, "c++")
     flag = util.compare_res.compile_and_compare(code2, code1)
@@ -83,7 +120,7 @@ def med_immi():
 
 def hard_immi():
     global times
-    code1 = util.problem_generator.codeGenerator(times, "hard", "memory")
+    code1 = util.problem_generator.codeGenerator(times, "hard", "space")
     times += 1
     times+=1
     code2 = util.immigration.codeImmigration(code1, "c++")
@@ -106,7 +143,22 @@ def main():
     time_data = []
     mem_data = []
     immi_data = []
-    while i < 10:
+    while i < 5:
+        print("new round")
+        print(i)
+        mem_data.append(easy_mem_prob())
+        time_data.append(easy_time_prob())
+
+        try:
+            immi_data.append(easy_immi())
+        except RuntimeError as e:
+            immi_data.append(["False", "Easy", "Immigration", "Python", "C++"])
+        print(time_data)
+        print(mem_data)
+        print(immi_data)
+        i += 1
+    i = 0
+    while i < 5:
         print("new round")
         print(i)
         mem_data.append(med_mem_prob())
@@ -121,7 +173,7 @@ def main():
         print(immi_data)
         i += 1
     i = 0
-    while i < 10:
+    while i < 5:
         print("new round")
         print(i)
         time_data.append(hard_time_prob())
